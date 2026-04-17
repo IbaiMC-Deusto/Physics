e2593c542ad3b6c8b8358d8c94498eec9e10e19d
IbaiMC-Deusto <ibai.mellado@opendeusto.es>
2026-04-17 09:44:38 +0200

    Jump added with control

--- Assets/Scripts/DinamicMovement.cs
+++ Assets/Scripts/DinamicMovement.cs
@@ -1,13 +1,21 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DinamicMovement : MonoBehaviour
 {
     public int JumpForce;
     public int velocity;
+    private bool canJump = true;
 
     void Update()
     {
         Movement();
+    }
+
+    private void FixedUpdate()
+    {
+        Jump();
     }
 
     void Movement()
@@ -31,6 +39,30 @@ public class DinamicMovement : MonoBehaviour
         {
             this.transform.position += Vector3.right * velocity;
         }
+    }
 
+    void Jump()
+    {
+        if (canJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 jumpVector = Vector3.up * JumpForce;
+            this.GetComponent<Rigidbody>().AddForce(jumpVector);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("ground"))
+        {
+            canJump = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("ground"))
+        {
+            canJump = false;
+        }
     }
 }
